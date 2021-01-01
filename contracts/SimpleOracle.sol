ce6b738eaff812692b14ce2902cbf70bd4d2d67e
Yash Agrawal <yashagrawal18@outlook.com>
2021-01-01 19:40:27 +0530

    Add simple oracle

---
+++ contracts/SimpleOracle.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+import './interfaces/ISimpleOracle.sol';
+
+contract SimpleOracle is Ownable, ISimpleOracle {
+    using SafeMath for uint256;
+
+    uint256 public price;
+
+    constructor() public {
+        // Set the initial price to 1.05.
+        price = uint256(105).mul(1e18).div(100);
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    function getPrice() public view returns (uint256) {
+        return price;
+    }
+
+    function consult(uint256 amountIn)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return price.mul(amountIn).div(1e18);
+    }
+
+    event Updated(uint256 price0CumulativeLast, uint256 price1CumulativeLast);
+}
