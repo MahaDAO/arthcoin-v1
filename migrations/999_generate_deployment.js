9b0812260b5c7aad5b6f13ecbaeb56d15bbe13cc
Yash Agrawal <yashagrawal18@outlook.com>
2021-01-05 18:41:51 +0530

    Minor changes

--- migrations/999_generate_deployment.js
+++ migrations/999_generate_deployment.js
@@ -10,7 +10,7 @@ function distributionPoolContracts() {
   return fs.readdirSync(path.resolve(__dirname, '../contracts/distribution'))
     .filter(filename => filename.endsWith('Pool.sol'))
     .filter(filename => !filename.includes('DAIBASLPTokenSharePool'))
-    .filter(filename => filename !== 'BACTOKENPool.sol' && filename !== 'DAITOKENLPTokenSharePool.sol')
+    .filter(filename => filename !== 'BACTOKENPool.sol')
     .map(filename => filename.replace('.sol', ''));
 }
 
