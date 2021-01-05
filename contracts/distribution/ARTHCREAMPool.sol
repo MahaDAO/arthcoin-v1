fbe70805bf06d1baa3c434ed18a76a8e169bed6f
Yash Agrawal <yashagrawal18@outlook.com>
2021-01-05 19:16:58 +0530

    Add more pools

---
+++ contracts/distribution/ARTHCREAMPool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import './BACTOKENPool.sol';
+
+contract ARTHCREAMPool is BACTOKENPool {
+    constructor(
+        address token0_,
+        address token1_,
+        uint256 starttime_
+    ) public BACTOKENPool(token0_, token1_, starttime_) {}
+}
