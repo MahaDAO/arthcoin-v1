50e2cee7e1f495d3ab5c4a1211d10fe4c00bb9f7
Yash Agrawal <yashagrawal18@outlook.com>
2021-01-05 20:15:26 +0530

    Add ropsten testnet addresses

--- migrations/known-contracts.js
+++ migrations/known-contracts.js
@@ -2,8 +2,8 @@
  * List known (already deployed and verified) contract addresses on here.
  */
 module.exports = {
+  // https://uniswap.org/docs/v2/smart-contracts/factory/#address
   UniswapV2Factory: {
-    // https://uniswap.org/docs/v2/smart-contracts/factory/#address
     mainnet: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
     ropsten: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
     rinkeby: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
@@ -15,17 +15,22 @@ module.exports = {
   },
   DAI: {
     mainnet: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    ropsten: '0xad6d458402f60fd3bd25163575031acdce07538d',
   },
   SUSD: {
     mainnet: '0x57Ab1E02fEE23774580C119740129eAC7081e9D3',
+    ropsten: '0xdd710d668df4d8871468c91c6366458e77ef7c38',
   },
   USDC: {
     mainnet: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    ropsten: '0x70cdfb73f78c51bf8a77b36c911d1f8c305d48e6',
   },
   USDT: {
     mainnet: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    ropsten: '0x6ee856ae55b6e1a249f04cd3b947141bc146273c',
   },
   yCRV: {
     mainnet: '0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8',
+    ropsten: '0xface8a2e245d91b90fce1e693d23eee688705025',
   },
 };
