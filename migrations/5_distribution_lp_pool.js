f53183fabc5ce240219e7391d58530209d74441c
Yash Agrawal <yashagrawal18@outlook.com>
2021-01-05 22:16:18 +0530

    Minor changes

--- migrations/5_distribution_lp_pool.js
+++ migrations/5_distribution_lp_pool.js
@@ -4,7 +4,7 @@ const MockDai = artifacts.require('MockDai');
 const SeigniorageOracle = artifacts.require('SeigniorageOracle');
 const DAIARTHLPToken_MAHAPool = artifacts.require('DAIARTHLPTokenSharePool')
 const UniswapV2Factory = artifacts.require('UniswapV2Factory');
-// const DAIBASLPToken_BASPool = artifacts.require('DAIBASLPTokenSharePool')
+// const DAIMAHALPToken_MAHAPool = artifacts.require('DAIMAHALPTokenSharePool')
 
 const knownContracts = require('./known-contracts');
 const { POOL_START_DATE } = require('./pools');
@@ -26,8 +26,8 @@ module.exports = async (deployer, network, accounts) => {
   const oracle = await SeigniorageOracle.deployed();
 
   const dai_arth_lpt = await oracle.pairFor(uniswapFactory.address, ARTH.address, dai.address);
-  // const dai_bas_lpt = await oracle.pairFor(uniswapFactory.address, MahaToken.address, dai.address);
+  // const dai_maha_lpt = await oracle.pairFor(uniswapFactory.address, MahaToken.address, dai.address);
 
   await deployer.deploy(DAIARTHLPToken_MAHAPool, MahaToken.address, dai_arth_lpt, POOL_START_DATE);
-  // await deployer.deploy(DAIBASLPToken_BASPool, MahaToken.address, dai_bas_lpt, POOL_START_DATE);
+  // await deployer.deploy(DAIMAHALPToken_MAHAPool, MahaToken.address, dai_maha_lpt, POOL_START_DATE);
 };
