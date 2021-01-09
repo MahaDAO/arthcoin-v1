d0228159a5067e1a033fb98f316bc7a43241c933
Steven Enamakel <enamakel@vezures.com>
2021-01-09 20:15:31 +0300

    fixed swap dai

--- contracts/SwapDAIForARTH.sol
+++ contracts/SwapDAIForARTH.sol
@@ -2,21 +2,26 @@
 
 pragma solidity ^0.6.0;
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol'
-
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import './lib/FixedPoint.sol';
 import './owner/Operator.sol';
+import './interfaces/IBasisAsset.sol';
+
 import './interfaces/IOracle.sol';
 
 contract SwapETHForTOKEN is Operator {
+    using FixedPoint for *;
+    using SafeMath for uint256;
+
     address public dai;
     address public cash;
     address public bond;
     address public oracle;
-    address public rewardRate;
+    uint256 public rewardRate;
     address public uniswapRouter;
 
-    constructor (
+    constructor(
         address dai_,
         address cash_,
         address bond_,
@@ -32,61 +37,43 @@ contract SwapETHForTOKEN is Operator {
         uniswapRouter = uniswapRouter_;
     }
 
-    function changeBondToken (address newBond) 
-        public 
-        onlyOperator 
-    {
-        require(newBondToken != address(0));
+    function changeBondToken(address newBond) public onlyOperator {
+        require(newBond != address(0));
 
         bond = newBond;
     }
 
-    function changeCashToken (address newCash) 
-        public 
-        onlyOperator 
-    {
+    function changeCashToken(address newCash) public onlyOperator {
         require(newCash != address(0));
 
         cash = newCash;
     }
 
-    function changeUniSwapRouter (address newUniswapRouter) 
-        public 
-        onlyOperator 
-    {
+    function changeUniSwapRouter(address newUniswapRouter) public onlyOperator {
         require(newUniswapRouter != address(0));
 
         uniswapRouter = newUniswapRouter;
     }
 
-    function changeOracle (address newOracle) 
-        public 
-        onlyOperator 
-    {
+    function changeOracle(address newOracle) public onlyOperator {
         require(newOracle != address(0));
 
         oracle = newOracle;
     }
 
-    function changeRewardRate (uint256 newRewardRate) 
-        public 
-        onlyOperator 
-    {
-        require(rewardRate >= 0);
-        require(rewardRate <= 100);
+    function changeRewardRate(uint256 newRewardRate) public onlyOperator {
+        require(newRewardRate >= 0);
+        require(newRewardRate <= 100);
 
         rewardRate = newRewardRate;
     }
 
-    function changeEntities (
-        address newCash, 
-        address newBond, 
+    function changeEntities(
+        address newCash,
+        address newBond,
         address newOracle,
         address newUniswapRouter
-    ) 
-        public 
-        onlyOperator 
-    {
+    ) public onlyOperator {
         require(newCash != address(0));
         require(newBond != address(0));
         require(newOracle != address(0));
@@ -98,36 +85,66 @@ contract SwapETHForTOKEN is Operator {
         uniswapRouter = newUniswapRouter;
     }
 
-    function swap(uint256 daiAmount) returns (uint[]) {
-        require(amount > 0);
-        
+    function swap(uint256 daiAmount) public {
+        require(daiAmount > 0);
+
         // Get price of dai.
-        uint256 daiPrice = IOracle(oracle).consult(dai.address, 1);
+        uint256 daiPrice = IOracle(oracle).consult(dai, 1);
         // Get price of cash.
-        uint256 cashPrice = IOracle(oracle).consult(cash.address, 1);
-        
+        uint256 cashPrice = IOracle(oracle).consult(cash, 1);
+
         // Eg. Let's say 1 dai(d) = 10 usd and 1 cash(c) = 20 usd.
         // Then taking c/d = 20/10 = 2.
         // Then c = 2d.
         uint256 expectedCashAmount = cashPrice.div(daiPrice);
-        
+
         uint256 rewardAmount = daiAmount.mul(rewardRate).div(100);
-        IERC20(bond).mint(msg.sender, rewardAmount);
+        IBasisAsset(bond).mint(msg.sender, rewardAmount);
 
         address[] memory path = new address[](2);
         path[0] = address(dai);
         path[1] = address(cash);
 
-        uint[] memory result = IUniswapV2Router02(uniswapRouter).swapExactTokensForTokens(
+        IUniswapV2Router02(uniswapRouter).swapExactTokensForTokens(
             daiAmount,
-            expectedCashAmount, 
-            path, 
-            msg.sender, 
+            expectedCashAmount,
+            path,
+            msg.sender,
             block.timestamp
         );
-        
+
         // TODO: Burn bought back arth.
-        
-        return result;
     }
+
+    // function buybackCashAndMintBond(uint256 daiAmount, uint256 bondDiscount) returns (uint[]) {
+    //     require(amount > 0);
+
+    //     // Get price of dai.
+    //     uint256 daiPrice = IOracle(oracle).consult(dai.address, 1);
+    //     // Get price of cash.
+    //     uint256 cashPrice = IOracle(oracle).consult(cash.address, 1);
+
+    //     // Eg. Let's say 1 dai(d) = 10 usd and 1 cash(c) = 20 usd.
+    //     // Then taking c/d = 20/10 = 2.
+    //     // Then c = 2d.
+    //     uint256 expectedCashAmount = cashPrice.div(daiPrice);
+
+    //     address[] memory path = new address[](2);
+    //     path[0] = address(dai);
+    //     path[1] = address(cash);
+
+    //     uint[] memory result = IUniswapV2Router02(uniswapRouter).swapExactTokensForTokens(
+    //         daiAmount,
+    //         expectedCashAmount,
+    //         path,
+    //         msg.sender,
+    //         block.timestamp
+    //     );
+
+    //     // TODO: Burn bought back arth.
+    //     // IBasisAsset(cash).burnFrom(msg.sender, expectedCashAmount);
+    //     // IBasisAsset(bond).mint(msg.sender, expectedCashAmount.mul(1e18).div(bondPrice));
+
+    //     return result;
+    // }
 }
