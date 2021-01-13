ee00d934eaeca88cc8d77243de205e0ef26cac18
Steven Enamakel <enamakel@vezures.com>
2021-01-13 20:46:54 +0300

    allow for dynamic epochs

--- contracts/utils/Epoch.sol
+++ contracts/utils/Epoch.sol
@@ -1,7 +1,6 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.6.0;
 
+import '@openzeppelin/contracts/math/Math.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
 
 import '../owner/Operator.sol';
@@ -11,7 +10,7 @@ contract Epoch is Operator {
 
     uint256 private period;
     uint256 private startTime;
-    uint256 private epoch;
+    uint256 private lastExecutedAt;
 
     /* ========== CONSTRUCTOR ========== */
 
@@ -20,9 +19,10 @@ contract Epoch is Operator {
         uint256 _startTime,
         uint256 _startEpoch
     ) public {
+        require(_startTime > block.timestamp, 'Epoch: invalid start time');
         period = _period;
         startTime = _startTime;
-        epoch = _startEpoch;
+        lastExecutedAt = startTime.add(_startEpoch.mul(period));
     }
 
     /* ========== Modifier ========== */
@@ -34,19 +34,41 @@ contract Epoch is Operator {
     }
 
     modifier checkEpoch {
-        require(now >= nextEpochPoint(), 'Epoch: not allowed');
+        require(now > startTime, 'Epoch: not started yet');
+        require(callable(), 'Epoch: not allowed');
 
         _;
 
-        epoch = epoch.add(1);
+        lastExecutedAt = block.timestamp;
     }
 
     /* ========== VIEW FUNCTIONS ========== */
 
-    function getCurrentEpoch() public view returns (uint256) {
-        return epoch;
+    function callable() public view returns (bool) {
+        return getCurrentEpoch() >= getNextEpoch();
     }
 
+    // epoch
+    function getLastEpoch() public view returns (uint256) {
+        return lastExecutedAt.sub(startTime).div(period);
+    }
+
+    function getCurrentEpoch() public view returns (uint256) {
+        return Math.max(startTime, block.timestamp).sub(startTime).div(period);
+    }
+
+    function getNextEpoch() public view returns (uint256) {
+        if (startTime == lastExecutedAt) {
+            return getLastEpoch();
+        }
+        return getLastEpoch().add(1);
+    }
+
+    function nextEpochPoint() public view returns (uint256) {
+        return startTime.add(getNextEpoch().mul(period));
+    }
+
+    // params
     function getPeriod() public view returns (uint256) {
         return period;
     }
@@ -55,13 +77,9 @@ contract Epoch is Operator {
         return startTime;
     }
 
-    function nextEpochPoint() public view returns (uint256) {
-        return startTime.add(epoch.mul(period));
-    }
-
     /* ========== GOVERNANCE ========== */
 
-    function setPeriod(uint256 _period) external onlyOwner {
+    function setPeriod(uint256 _period) external onlyOperator {
         period = _period;
     }
 }
