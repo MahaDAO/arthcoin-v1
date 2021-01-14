27e894021ac2e692ef58280185699a7f6ba0a5db
Steven Enamakel <enamakel@vezures.com>
2021-01-14 11:28:15 +0300

    removed operator check on the migrate function

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -86,7 +86,7 @@ contract Treasury is TreasurySetters {
         emit Initialized(msg.sender, block.number);
     }
 
-    function migrate(address target) public onlyOperator checkOperator {
+    function migrate(address target) public onlyOperator {
         require(!migrated, 'Treasury: migrated');
 
         // cash
