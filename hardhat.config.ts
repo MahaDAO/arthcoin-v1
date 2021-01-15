1a5728980574c73b07021e87899a3d2b83c85f68
Steven Enamakel <enamakel@vezures.com>
2021-01-15 12:53:20 +0300

    added multi token reward distributor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,14 +5,14 @@ import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 
-
-require('dotenv').config();
-
-
 export default {
   default: 'development',
   networks: {
     hardhat: {},
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.WALLET_SECRET_KEY}`]
+    },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.WALLET_SECRET_KEY}`]
