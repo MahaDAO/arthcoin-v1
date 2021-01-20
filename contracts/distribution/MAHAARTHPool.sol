e5f09641374fa64cb5b325d1b128812533698345
Steven Enamakel <enamakel@vezures.com>
2021-01-20 17:31:25 +0300

    update solidity smart contracts

--- contracts/distribution/MAHAARTHPool.sol
+++ contracts/distribution/MAHAARTHPool.sol
@@ -8,19 +8,6 @@ contract MAHAARTHPool is ARTHTOKENPool {
     constructor(
         address cash_,
         address dai_,
-        uint256 starttime_,
-        uint256 maxPoolSize_,
-        bool limitPoolSize_,
-        string memory poolName_
-    )
-        public
-        ARTHTOKENPool(
-            cash_,
-            dai_,
-            starttime_,
-            maxPoolSize_,
-            limitPoolSize_,
-            'MAHAARTHPool'
-        )
-    {}
+        uint256 starttime_
+    ) public ARTHTOKENPool(cash_, dai_, starttime_, 0, false, 'MAHAARTHPool') {}
 }
--- contracts/distribution/MAHAMAHAETHLPTokenPool.sol
+++ contracts/distribution/MAHAMAHAETHLPTokenPool.sol
@@ -8,18 +8,15 @@ contract MAHAMAHAETHLPTokenPool is ARTHTOKENPool {
     constructor(
         address cash_,
         address dai_,
-        uint256 starttime_,
-        uint256 maxPoolSize_,
-        bool limitPoolSize_,
-        string memory poolName_
+        uint256 starttime_
     )
         public
         ARTHTOKENPool(
             cash_,
             dai_,
             starttime_,
-            maxPoolSize_,
-            limitPoolSize_,
+            0,
+            false,
             'MAHAMAHAETHLPTokenPool'
         )
     {}
