e68813ef848b23d2890a11b8cbb70dbcbc9036bd
Yash Agrawal <yashagrawal18@outlook.com>
2021-01-21 21:05:29 +0530

    Minor changes

--- test/Treasury.test.ts
+++ test/Treasury.test.ts
@@ -624,45 +624,6 @@ describe('Treasury', () => {
           expect(await bond.balanceOf(ant.address)).to.gt(ZERO);
         });
 
-        it('should work if cashToBondConversionLimit < boughtBackCash but not 0', async () => {
-          const cashPrice = ETH.mul(90).div(100); // $0.90
-          await oracle.setPrice(cashPrice);
-          await oracle.setEpoch(1);
-
-          // trigger updateConversionRate
-          await treasury.allocateSeigniorage();
-
-          const path = [dai.address, cash.address];
-          const amountsOut = await uniswapRouter.getAmountsOut(ETH, path);
-          const expectedCashAmount = amountsOut[1];
-          const output = await uniswapRouter.swapExactTokensForTokens(
-            ETH,
-            expectedCashAmount,
-            path,
-            ant.address,
-            BigNumber.from((await latestBlocktime(provider))).add(60)
-          );
-          console.log(output);
-
-          const boughtBackCash = bigmin(output[1], expectedCashAmount);
-
-          console.log(boughtBackCash.toString());
-          console.log((await treasury.cashToBondConversionLimit()).toString());
-
-          await dai.connect(operator).transfer(ant.address, ETH);
-          await dai.connect(ant).approve(treasury.address, ETH);
-          await cash.connect(ant).approve(treasury.address, ETH);
-
-          expect(treasury.connect(ant).buyBonds(ETH, cashPrice))
-          // .to.emit(treasury, 'BoughtBonds')
-          // // TODO: calculate real numbers
-          // .withArgs(ant.address, ETH, BigNumber.from("906610893880149131"), BigNumber.from("915768579676918314"));
-
-          expect(await dai.balanceOf(ant.address)).to.eq(ZERO);
-          // TODO: use a proper number;
-          expect(await bond.balanceOf(ant.address)).to.gt(ZERO);
-        });
-
         it('should fail if cash price over $1 but inside band', async () => {
           const cashPrice = ETH.mul(101).div(100); // $1.01
           await oracle.setPrice(cashPrice);
