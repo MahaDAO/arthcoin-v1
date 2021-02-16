44507c1b0506b9a8e1627849d940ae2f4caa8d74
Steven Enamakel <enamakel@vezures.com>
2021-02-17 02:09:15 +0300

    added the vaults

---
+++ contracts/boardoom/v2/VaultArthMlp.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import '../core/Vault.sol';
+
+contract VaultArthMlp is Vault {
+    constructor(IERC20 cash_, uint256 lockIn_) Vault(cash_, lockIn_) {}
+}
