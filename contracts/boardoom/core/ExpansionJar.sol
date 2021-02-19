85cb7ebcbf014c58cb22c1d7002c6674314b9e90
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-19 23:43:14 +0530

    Minor changes

--- contracts/boardoom/core/ExpansionJar.sol
+++ contracts/boardoom/core/ExpansionJar.sol
@@ -17,12 +17,13 @@ contract ExpansionJar is Epoch {
     IERC20 token;
     VestedVaultBoardroom boardroom;
 
+    uint256 public totalReward;
+    uint256 internal _totalSupply;
     uint256 compoundFor = 30 days;
     uint256 harvestAfter = 5 days;
-
     bool enableWithdrawal = false;
+    uint256 public totalAmountThatWasStaked;
 
-    uint256 internal _totalSupply;
     mapping(address => uint256) internal _balances;
 
     modifier stakerExists(address who) {
@@ -73,9 +74,32 @@ contract ExpansionJar is Epoch {
         _balances[msg.sender] = _balances[msg.sender].add(amount);
     }
 
+    function unbond() public onlyOwner checkStartTime {
+        if (block.timestamp >= startTime.add(compoundFor)) {
+            uint256 balance = vault.balanceOf(address(this));
+
+            vault.unbond(balance);
+
+            // Since this is the balance, that we have after compouding for compouding duration.
+            // Hence this also has the principal amount, which we subtract to get only epochly
+            // reward which we would have claimed.
+            totalReward = totalReward.add(balance).sub(_totalSupply);
+        }
+    }
+
+    function harvest() public onlyOwner checkStartTime {
+        if (block.timestamp >= startTime.add(compoundFor).add(harvestAfter))
+            enableWithdrawal = true;
+
+        vault.withdraw();
+
+        // HERE we don't calcualte the reward once again as the boardroom
+        // considers balance that is still bonded. However we unbond only once with full balance
+        // after the compound period, hence the balance of still bonded would be 0.
+    }
+
     function claimAndReinvest() public onlyOwner checkEpoch checkStartTime {
-        if (getCurrentEpoch() >= getNextEpoch() && getNextEpoch() > 0)
-            boardroom.claimAndReinvestReward();
+        boardroom.claimAndReinvestReward();
 
         if (block.timestamp >= startTime.add(compoundFor).add(harvestAfter))
             enableWithdrawal = true;
@@ -85,10 +109,15 @@ contract ExpansionJar is Epoch {
         require(enableWithdrawal, 'Jar: too early');
 
         uint256 amount = _balances[msg.sender];
+        uint256 percentOfStake =
+            amount.mul(100).mul(1e18).div(totalAmountThatWasStaked);
+
+        uint256 amountToReward =
+            totalReward.mul(percentOfStake).div(100).div(1e18);
 
         _balances[msg.sender] = 0;
         _totalSupply = _totalSupply.sub(amount);
 
-        token.transfer(msg.sender, amount);
+        token.transfer(msg.sender, amountToReward);
     }
 }
