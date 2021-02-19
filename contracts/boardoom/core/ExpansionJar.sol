4cc5fc6ea800c0172099549038641645e1ecbf38
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-19 22:38:30 +0530

    Add Epoch and minor changes

--- contracts/boardoom/core/ExpansionJar.sol
+++ contracts/boardoom/core/ExpansionJar.sol
@@ -6,22 +6,29 @@ import {SafeMath} from '@openzeppelin/contracts/contracts/math/SafeMath.sol';
 import {IERC20} from '@openzeppelin/contracts/contracts/token/ERC20/IERC20.sol';
 
 import {Vault} from './Vault.sol';
+import {Epoch} from '../../utils/Epoch.sol';
 import {Operator} from '../../owner/Operator.sol';
 import {VestedVaultBoardroom} from './VestedVaultBoardroom.sol';
-import {StakingTimelock} from '../../timelock/StakingTimelock.sol';
 
-contract ExpansionJar is StakingTimelock {
+contract ExpansionJar is Epoch {
     using SafeMath for uint256;
 
     Vault vault;
     IERC20 token;
     VestedVaultBoardroom boardroom;
 
+    uint256 startTime;
+    uint256 harvestingDuration = 30 days;
+    uint256 compoundingDuration = 5 days;
+
+    bool enableWithdrawal = false;
+
     uint256 internal _totalSupply;
     mapping(address => uint256) internal _balances;
 
     modifier stakerExists(address who) {
         require(balanceOf(who) > 0, 'Boardroom: The director does not exist');
+
         _;
     }
 
@@ -29,11 +36,15 @@ contract ExpansionJar is StakingTimelock {
         IERC20 token_,
         Vault vault_,
         VestedVaultBoardroom boardroom_,
-        uint256 duration_
-    ) public StakingTimelock(duration_) {
+        uint256 _startTime,
+        uint256 _period,
+        uint256 _startEpoch
+    ) public Epoch(_period, _startTime, _startEpoch) {
         vault = vault_;
         token = token_;
         boardroom = boardroom_;
+
+        startTime = block.timestamp;
     }
 
     function totalSupply() public view returns (uint256) {
@@ -44,7 +55,7 @@ contract ExpansionJar is StakingTimelock {
         return _balances[who];
     }
 
-    function bond(uint256 amount) public {
+    function bond(uint256 amount) public checkStartTime {
         require(amount > 0, 'Jar: amount = 0');
 
         vault.bond(amount);
@@ -53,26 +64,19 @@ contract ExpansionJar is StakingTimelock {
         _balances[msg.sender] = _balances[msg.sender].add(amount);
     }
 
-    function unbond(uint256 amount) public stakerExists(msg.sender) {
-        uint256 directorShare = _balances[msg.sender];
+    function claimAndReinvest() public onlyOwner checkEpoch {
+        if (getCurrentEpoch() >= getNextEpoch() && getNextEpoch() > 0)
+            boardroom.claimAndReinvestReward();
 
-        require(
-            directorShare >= amount,
-            'Jar: unbond request greater than staked amount'
-        );
-
-        _updateStakerDetails(mgs.sender, block.timestamp + duration, amount);
+        if (
+            block.timestamp >=
+            startTime.add(compoundingDuration).add(harvestingDuration)
+        ) enableWithdrawal = true;
     }
 
-    function claimAndReinvest() public onlyOwner {
-        boardroom.claimAndReinvestReward();
-    }
+    function withdraw() public stakerExists(msg.sender) checkStartTime {
+        require(enableWithdrawal, 'Jar: too early');
 
-    function withdraw()
-        public
-        stakerExists(msg.sender)
-        checkLockDurationFor(msg.sender)
-    {
         uint256 directorShare = _balances[msg.sender];
         uint256 amount = getStakedAmount(msg.sender);
 
