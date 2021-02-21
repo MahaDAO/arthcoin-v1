bf78135a7acd40e621a0b5a7ff322e4bc6d29260
Steven Enamakel <enamakel@vezures.com>
2021-02-21 15:45:55 +0300

    fixed broken helpers

--- migrations/helpers.js
+++ migrations/helpers.js
@@ -6,9 +6,7 @@ const getDAI = async (network, deployer, artifacts) => {
 
   const addr = knownContracts.DAI[network];
   if (addr) return IERC20.at(addr);
-
-  const found = MockDai.deployed();
-  if (found) return found;
+  if (MockDai.isDeployed()) return MockDai.deployed();
 
   console.log(`Deploying mock dai on ${network} network.`);
   await deployer.deploy(MockDai);
@@ -23,13 +21,12 @@ const getMahaToken = async (network, deployer, artifacts) => {
   const addr = knownContracts.MahaToken[network];
   if (addr) return IERC20.at(addr);
 
-  const found = MahaToken.deployed();
-  if (found) return found;
+  if (MahaToken.isDeployed()) return MahaToken.deployed();
 
   console.log(`Deploying mahatoken on ${network} network.`);
   await deployer.deploy(MahaToken);
   return MahaToken.deployed();
-}
+};
 
 
 const getUniswapFactory = async (network, deployer, artifacts) => {
@@ -38,13 +35,12 @@ const getUniswapFactory = async (network, deployer, artifacts) => {
   const addr  = knownContracts.UniswapV2Factory[network];
   if (addr) return UniswapV2Factory.at(addr);
 
-  const found = UniswapV2Factory.deployed();
-  if (found) return found;
+  if (UniswapV2Factory.isDeployed()) return UniswapV2Factory.deployed();
 
   console.log(`Deploying uniswap factory on ${network} network.`);
   await deployer.deploy(UniswapV2Factory, '0x0000000000000000000000000000000000000000');
   return UniswapV2Factory.deployed();
-}
+};
 
 
 const getUniswapRouter = async (network, deployer, artifacts) => {
@@ -53,12 +49,11 @@ const getUniswapRouter = async (network, deployer, artifacts) => {
   const addr  = knownContracts.UniswapV2Router02[network];
   if (addr) return UniswapV2Router02.at(addr);
 
-  const found = UniswapV2Router02.deployed();
-  if (found) return found;
+  if (UniswapV2Router02.isDeployed()) return UniswapV2Router02.deployed();
 
   console.log(`Deploying uniswap router on ${network} network.`);
-  const factory = await getUniswapFactory(network, deployer, artifacts)
-  await deployer.deploy(UniswapV2Router02, factory.address, accounts[0]);
+  const factory = await getUniswapFactory(network, deployer, artifacts);
+  await deployer.deploy(UniswapV2Router02, factory.address, '0x0000000000000000000000000000000000000000');
   return UniswapV2Router02.deployed();
 };
 
@@ -66,7 +61,7 @@ const approveIfNot = async (token, spender, amount) => {
   console.log(` - Approving ${token.symbol ? (await token.symbol()) : token.address}`);
   await token.approve(spender, amount);
   console.log(` - Approved ${token.symbol ? (await token.symbol()) : token.address}`);
-}
+};
 
 
 const getPairAddress = async (token1, token2, network, deployer, artifacts) => {
